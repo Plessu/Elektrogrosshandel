7fd6d995c6e5cf052f418b1a76da4feae4416cae
Plessu <giacomograef@gmail.com>
2025-12-03 10:50:19 +0100

    Update VS workspace and build output files
    Renamed and updated several .vsidx files in the VSCode workspace, modified DocumentLayout.json to reflect changes in document order and properties, and updated build output files in obj/Debug/net10.0 to reflect the latest build state.

--- VSCode/Elektrogrrosshandel/Elektrogrrosshandel/obj/Debug/net10.0/Elektrogrrosshandel.AssemblyInfo.cs
+++ VSCode/Elektrogrrosshandel/Elektrogrrosshandel/obj/Debug/net10.0/Elektrogrrosshandel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Elektrogrrosshandel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1721ada66752f532638d525f907ca884d48ed172")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3def8750e662282889931ca82243632d990cbfbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Elektrogrrosshandel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Elektrogrrosshandel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
