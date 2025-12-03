2592400ce09da7feb0a4a0a5f9690592a5c05e75
Plessu <giacomograef@gmail.com>
2025-12-03 18:39:33 +0100

    Introducing Improved Article Logic
    Simpler more efficent way to Handel Aritcels and there Properties by removing IArticle and other Interfaces and introdued new GraphicCard Class and ParentArticleGroupClass

---
+++ VSCode/Elektrogrrosshandel/Elektrogrrosshandel/ArticelGroup_GraphicCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elektrogrrosshandel
+{
+    internal class ArticelGroup_GraphicCard
+    {
+        private string ArticelGroupName = "Graphic Card";
+        private int ArticelGroupID = 100;
+        private List<ArticelGroup_GraphicCard> GraphicCards { get; set; }
+
+
+    }
+}
---
+++ VSCode/Elektrogrrosshandel/Elektrogrrosshandel/ArticelParentGroup_Computer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elektrogrrosshandel
+{
+    internal class ArticelParentGroup_Computer
+    {
+        public static string ArticelParentGroup = "Computer";
+        private static int ArticelParentGroupID = 10;
+
+        public int GetArticelParentGroupID()
+        {
+            return ArticelParentGroupID;
+        }
+    }
+}
