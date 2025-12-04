ae71a7a34bd2434543429c1dd0229928edec50e0
Plessu <giacomograef@gmail.com>
2025-12-04 14:52:51 +0100

    v0.3

--- VSCode/Elektrogrrosshandel/Elektrogrrosshandel/obj/Debug/net10.0/Elektrogrrosshandel.AssemblyInfo.cs
+++ VSCode/Elektrogrrosshandel/Elektrogrrosshandel/obj/Debug/net10.0/Elektrogrrosshandel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Elektrogrrosshandel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+072bf35b402148d327e0c7cf38112483524da1b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b73582f2a48fd99f44f50f63a18b1c44d12f8e59")]
 [assembly: System.Reflection.AssemblyProductAttribute("Elektrogrrosshandel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Elektrogrrosshandel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
